3f6c14810dde417345ab9cd3434f069ed1bb9aa6
samantha gard <samantha.gard13@gmail.com>
2023-06-14 10:10:20 -0400

    changes to javascript and css

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,34 @@
-var timer = document.timer
+
+// timer starts when button is pressed
+//generatebtn
+
+var timerElement = document.querySelector("timer");
+var startButton = document.querySelector("startbutton");
+startButton.addEventListener("click", timerFunction);
+
+
+var seconds = 0;
+var minutes = 0;
+
+function timerFunction() {
+    seconds++;
+
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if (minutes == 5) {
+        return;
+    }
+
+timerElement.textContent = "Timer" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+
+scoreScreen();
+}
+
+var quiz = document.querySelector(".questions");
+
+function quiz() {
+    
+}
+
