976acf34ad508b83f4a1ed7482948b8e9f3199d1
samantha gard <samantha.gard13@gmail.com>
2023-06-15 15:38:16 -0400

    additions to js and html

--- Assets/script.js
+++ Assets/script.js
@@ -1,34 +1,75 @@
-
-// timer starts when button is pressed
-//generatebtn
-
+// Assignments for each section
 let timerElement = document.querySelector("#timer");
-let startButton = document.querySelector(".startButton");
+let startButton = document.getElementById("startbutton");
 let quiz = document.querySelector(".questions");
-startButton.addEventListener("click", timerFunction);
 
-document.getElementById("card2").style.display = "none";
-document.getElementById("card3").style.display = "none";
-document.getElementById("card4").style.display = "none";
-document.getElementById("card5").style.display = "none";
-document.getElementById("card6").style.display = "none";
+// Timer Function
+startButton.addEventListener("click", startTimer);
 
-let seconds = 0;
-let minutes = 0;
+let seconds = 59;
+let minutes = 4;
+let interval = null;
+
+function startTimer() {
+    interval = setInterval(timerFunction, 1000);
+}
 
 function timerFunction() {
-    seconds++;
+    seconds--;
 
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
+    if (seconds == 0) {
+        seconds = 59;
+        minutes--;
     }
-    if (minutes == 2) {
+    if (minutes == 0 && seconds == 0) {
         return;
     }
 
-timerElement.textContent = "Timer" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+timerElement.textContent = "Timer " + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
 
 scoreScreen();
 }
 
+// Page Switching Quiz/ Wrong answer time is added
+
+let alertMessage = document.getElementById("wronganswer")
+
+let card = document.getElementById("card");
+let card2 = document.getElementById("card2");
+let card3 = document.getElementById("card3");
+let card4 = document.getElementById("card4");
+let card5 = document.getElementById("card5");
+let card6 = document.getElementById("card6");
+
+let question1 = document.getElementById("form1");
+let correctAnswer1 = document.getElementById("form1"[1])
+let question2 = document.getElementById("form2");
+let question3 = document.getElementById("form3");
+let question4 = document.getElementById("form4");
+let question5 = document.getElementById("form5");
+
+startButton.addEventListener("click", quizTime);
+
+function quizTime() {
+    card.style.display = "none";
+    card2.style.display = "block";
+
+    if (selected === correctAnswer1) {
+        score++;
+    }
+    if (selected === incorrectAnswer) {
+        seconds= -30;
+        alertMessage.style.display = "block";
+    }
+}
+
+
+
+
+
+/* Score Tracking and Display
+let score = 0
+let bestTime = 0
+
+function scoreScreen
+*/
