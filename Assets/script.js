80d0716ee0574182af595056cbaa0834490eff1b
samantha gard <samantha.gard13@gmail.com>
2023-06-25 11:55:19 -0400

    js fixes

--- Assets/script.js
+++ Assets/script.js
@@ -5,13 +5,14 @@ let mainMenu = document.getElementById("card");
 let scoreBoard = document.getElementById("scores");
 let scoreCard = document.getElementById("scorecard");
 let addScore = document.getElementById("addscore");
+let highscores = document.querySelector(".highscores");
 let initialsInput = document.getElementById("initialsinput");
-let initialSubmit = document.getElementById("initialsubmit");
+let initialsSubmit = document.getElementById("initialssubmit");
 let initialDisplay = document.getElementById("initialdisplay");
 let questionTitle = document.getElementById("questionTitle");
 let questionsCard = document.getElementById("questionscard");
-let seconds = 59;
-let minutes = 4;
+let seconds = 60;
+let minutes = 2;
 let interval = null;
 let questionsArray = [
     "Which One Of These Logs To The Console?",
@@ -20,59 +21,68 @@ let questionsArray = [
     "How Do You Add A Paragraph Element To HTML?",
     "What Word Can You Use To Declare A Variable In JavaScript?"];
 let currentQuestionIndex = 0;
-let choice1Array = ["1. consolelog.", "1. git shove", "1. ul", "1. <p>", "1. let"];
-let choice2Array = ["2. console.log", "2. git jump", "2. il", "2. <P>", "2. var"];
-let choice3Array = ["3. consoleLog", "3. git push", "3. ls", "3. <par>", "3. const"];
-let choice4Array = ["4. None", "4. None", "4. None", "4. None", "4. All Of The Above"];
-let correctAnswers = ["2. console.log", "3. git push", "3. ls", "2. <p>", "4. All Of The Above"];
+let choice1Array = [" consolelog. ", " git shove ", " ul ", " <p> ", " let "];
+let choice2Array = [" console.log ", " git jump ", " il ", " <P> ", " var "];
+let choice3Array = [" consoleLog ", " git push ", " ls ", " <par> ", " const "];
+let choice4Array = [" None ", " None ", " None ", " None ", " All Of The Above "];
+let correctAnswers = [" console.log ", " git push ", " ls ", " <p> ", " All Of The Above "];
 let choice1Index = 0;
 let choice2Index = 0;
 let choice3Index = 0;
 let choice4Index = 0;
-let options = document.querySelector(".choices");
+let options = document.querySelectorAll(".choices");
 let alertMessage = document.querySelector("#answeralert");
 
 function startQuiz() {
 mainMenu.style.display = "none";
 questionsCard.style.display = "block";
- nextQuestion();
- startTimer();
-}
+startTimer();
+nextQuestion();
+};
 
 function nextQuestion() {
-event.preventDefault()
-alertMessage.style.display = "none";
-//display first question of array
-questionTitle.textContent = questionsArray[currentQuestionIndex];
-//display first of all answer array
-choice1.textContent = choice1Array[currentQuestionIndex];
-choice2.textContent = choice2Array[currentQuestionIndex];
-choice3.textContent = choice3Array[currentQuestionIndex];
-choice4.textContent = choice4Array[currentQuestionIndex];
+    event.preventDefault()
+    questionTitle.textContent = questionsArray[currentQuestionIndex];
+    choice1.textContent = choice1Array[currentQuestionIndex];
+    choice2.textContent = choice2Array[currentQuestionIndex];
+    choice3.textContent = choice3Array[currentQuestionIndex];
+    choice4.textContent = choice4Array[currentQuestionIndex];
+    console.log("working1");
 
-    if (options.selected === correctAnswers[currentQuestionIndex]) {
-       currentQuestionIndex++;
-       choice1Index++;
-       choice2Index++;
-       choice3Index++;
-       choice4Index++;
-    } else {
-        seconds -= 30;
-        alertMessage.style.display = "block";
+    for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", checkAnswer);
+    }
+};
+
+function checkAnswer(event) {
+    var selectedAnswer = event.target.innerText.trim();
+    var correctAnswer = correctAnswers[currentQuestionIndex];
+    console.log(event.target.innerText);
+    
+    if (selectedAnswer === correctAnswer) {
         currentQuestionIndex++;
         choice1Index++;
         choice2Index++;
         choice3Index++;
         choice4Index++;
-    }
+        console.log("working2")
+        nextQuestion();
+    } else {
+        alertMessage.style.display = "block";
+        seconds -= 30;
+        currentQuestionIndex++;
+        choice1Index++;
+        choice2Index++;
+        choice3Index++;
+        choice4Index++;
+        console.log("working3");
 
-    if (minutes === 0 && seconds == 0) {
-        addInitials();
+        setTimeout(function() {
+            alertMessage.style.display = "none";
+        }, 3000);
     }
-    if (currentQuestionIndex == 5) {
-        addInitials();
-    }
-}
+    nextQuestion();
+};
 
 function startTimer() {
     interval = setInterval(timerFunction, 1000);
@@ -83,7 +93,7 @@ function startTimer() {
 function timerFunction() {
     seconds--;
 
-    if (seconds === 0) {
+    if (seconds <= 0) {
         seconds = 59;
         minutes--;
     }
@@ -94,21 +104,37 @@ function timerFunction() {
     }
 
 timerElement.textContent = "Timer " + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+
+    if (minutes === 0 && seconds == 0) {
+        clearInterval(interval);
+        addInitials();
+    }
+    if (currentQuestionIndex >= 5) {
+        localStorage.setItem("time", timerElement.textContent);
+        clearInterval(interval);
+        addInitials();
+    }
 };
 
 function addInitials() {
+    scoreCard.style.display = "block";
     addScore.style.display = "block";
-    initialSubmit.addEventListener("click", scoreScreen);
-}
+    timerElement.style.display = "block";
+    highscores.style.display = "none";
+    questionsCard.style.display = "none";
+    mainMenu.style.display = "none";
+    initialsSubmit.addEventListener("click", scoreScreen);
+};
 
 function scoreScreen() {
-    scoreCard.style.display = "block";
+    highscores.style.display = "block";
     mainMenu.style.display = "none";
     questionsCard.style.display = "none";
     addScore.style.display = "none";
+    let time = localStorage.getItem("time");
+    timerElement.textContent = "Time Left: " + time;
     let response = initialsInput.value;
     initialDisplay.textContent = response + " " + timerElement.textContent;
-    // add to previous list
 };
 
 scoreBoard.addEventListener("click", scoreScreen);
